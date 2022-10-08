3fae9c8aceed45cf3517a163208e7f234220d7ca
Casper Schuijt <casperschuijt@gmail.com>
2022-10-08 20:39:56 +0200

    tests for ctype functions

--- test.c
+++ test.c
@@ -6,13 +6,14 @@
 /*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/08 17:48:56 by cschuijt      #+#    #+#                 */
-/*   Updated: 2022/10/08 19:45:03 by cschuijt      ########   odam.nl         */
+/*   Updated: 2022/10/08 20:34:09 by cschuijt      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "munit.h"
 #include "libft.h"
 #include <stdlib.h>
+#include <ctype.h>
 
 MunitResult	ft_atoi_test(const MunitParameter params[], void *data)
 {
@@ -36,10 +37,115 @@ MunitResult	ft_atoi_test(const MunitParameter params[], void *data)
 	return (MUNIT_OK);
 }
 
+MunitResult	ft_isalnum_test(const MunitParameter params[], void *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < 128)
+	{
+		munit_assert_int((isalnum(i) > 0), ==, (ft_isalnum(i) > 0));
+		i++;
+	}
+	return (MUNIT_OK);
+}
+
+MunitResult	ft_isalpha_test(const MunitParameter params[], void *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < 128)
+	{
+		munit_assert_int((isalpha(i) > 0), ==, (ft_isalpha(i) > 0));
+		i++;
+	}
+	return (MUNIT_OK);
+}
+
+MunitResult	ft_isascii_test(const MunitParameter params[], void *data)
+{
+	int	i;
+
+	i = -1;
+	while (i < 180)
+	{
+		munit_assert_int((isascii(i) > 0), ==, (ft_isascii(i) > 0));
+		i++;
+	}
+	return (MUNIT_OK);
+}
+
+MunitResult	ft_isdigit_test(const MunitParameter params[], void *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < 128)
+	{
+		munit_assert_int((isdigit(i) > 0), ==, (ft_isdigit(i) > 0));
+		i++;
+	}
+	return (MUNIT_OK);
+}
+
+MunitResult	ft_isprint_test(const MunitParameter params[], void *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < 128)
+	{
+		munit_assert_int((isprint(i) > 0), ==, (ft_isprint(i) > 0));
+		i++;
+	}
+	return (MUNIT_OK);
+}
+
 MunitTest tests[] = {
 {
 	"/ft_atoi",
 	ft_atoi_test,
+	NULL,
+	NULL,
+	MUNIT_TEST_OPTION_NONE,
+	NULL
+},
+{
+	"/ft_isalnum",
+	ft_isalnum_test,
+	NULL,
+	NULL,
+	MUNIT_TEST_OPTION_NONE,
+	NULL
+},
+{
+	"/ft_isalpha",
+	ft_isalpha_test,
+	NULL,
+	NULL,
+	MUNIT_TEST_OPTION_NONE,
+	NULL
+},
+{
+	"/ft_isascii",
+	ft_isascii_test,
+	NULL,
+	NULL,
+	MUNIT_TEST_OPTION_NONE,
+	NULL
+},
+{
+	"/ft_isdigit",
+	ft_isdigit_test,
+	NULL,
+	NULL,
+	MUNIT_TEST_OPTION_NONE,
+	NULL
+},
+{
+	"/ft_isprint",
+	ft_isprint_test,
 	NULL,
 	NULL,
 	MUNIT_TEST_OPTION_NONE,
