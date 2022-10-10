7282cef0cb27c55d294c563deb7c3373132e7861
Casper Schuijt <casperschuijt@gmail.com>
2022-10-10 23:17:34 +0200

    Fix ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/10 14:28:09 by cschuijt      #+#    #+#                 */
-/*   Updated: 2022/10/10 18:40:11 by cschuijt      ########   odam.nl         */
+/*   Updated: 2022/10/10 23:15:25 by cschuijt      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,22 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	n;
 	size_t	len;
 
-	len = ft_strlen(src) + ft_strlen(dst);
 	n = 0;
-	if (dstsize > 0)
+	len = ft_strlen(dst) + ft_strlen(src);
+	while (*dst)
 	{
-		while (*dst)
-		{
-			dst++;
-			n++;
-		}
-		while (*src && n < dstsize - 1)
-		{
-			*dst = *src;
-			dst++;
-			src++;
-			n++;
-		}
-		if (dstsize > 0)
-			*dst = '\0';
+		if (n == dstsize)
+			return (ft_strlen(src) + dstsize);
+		dst++;
+		n++;
 	}
+	while (n < (dstsize - 1) && *src)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+		n++;
+	}
+	*dst = 0;
 	return (len);
 }
